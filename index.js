79f248b0c15f9f5d1258534cbd94bed7cd411eaa
GabrielCoceress998 <coceres9988@gmail.com>
2022-10-27 17:01:39 -0300

    ya responsive

---
+++ index.js
@@ -0,0 +1,5 @@
+const navButton= document.querySelector(".button")
+const navMenu= document.querySelector(".nav-menu")
+
+navButton.addEventListener("click", ()=> {
+	navMenu.classList.toggle("nav-menu_visible")});
